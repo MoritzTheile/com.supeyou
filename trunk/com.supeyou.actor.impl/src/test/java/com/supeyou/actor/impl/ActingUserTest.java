8e05ee97adecb8e28f30bb979e38f2fba09b9f7c
MoritzTheile <MoritzTheile@757eb1d2-deeb-4e35-a037-182455f4f83f>
2015-05-20 12:57:14 +0000

    added test for ActingUser

---
+++ trunk/com.supeyou.actor.impl/src/test/java/com/supeyou/actor/impl/ActingUserTest.java
@@ -0,0 +1,17 @@
+package com.supeyou.actor.impl;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.supeyou.actor.impl.initialdata.InitialActorData;
+import com.supeyou.crudie.iface.datatype.CRUDException;
+
+public class ActingUserTest {
+
+	@Test
+	public void test() throws CRUDException {
+
+		Assert.assertNotNull(InitialActorData.i().session1);
+	}
+
+}
