c4ced2d338ddb7a22564127abce42f59e97e9b72
MoritzTheile <MoritzTheile@757eb1d2-deeb-4e35-a037-182455f4f83f>
2015-05-20 12:57:14 +0000

    added test for ActingUser

--- com.supeyou.actor.impl/src/main/java/com/supeyou/actor/impl/initialdata/InitialActorData.java
+++ com.supeyou.actor.impl/src/main/java/com/supeyou/actor/impl/initialdata/InitialActorData.java
@@ -1,6 +1,8 @@
 package com.supeyou.actor.impl.initialdata;
 
+import com.supeyou.actor.iface.dto.ActingUserDTO;
 import com.supeyou.actor.iface.dto.SessionDTO;
+import com.supeyou.actor.impl.ActingUserCRUDServiceImpl;
 import com.supeyou.actor.impl.SessionCRUDServiceImpl;
 import com.supeyou.crudie.iface.datatype.CRUDException;
 import com.supeyou.crudie.iface.datatype.types.SingleLineString256Type;
@@ -14,11 +16,22 @@ public class InitialActorData {
 	public SessionDTO session1;
 	public SessionDTO session2;
 
+	public ActingUserDTO actingUser1;
+
 	private void init() throws CRUDException {
 
 		admin = UserCRUDServiceImpl.i().getInitialAdmin();
 		session1 = create("ajkajsdf", "78345919");
 		session2 = create("erze8ak", "837862992");
+		actingUser1 = create("");
+
+	}
+
+	private ActingUserDTO create(String string) throws CRUDException {
+
+		ActingUserDTO dto = new ActingUserDTO();
+		dto.setLoginId(new SingleLineString256Type(string));
+		return ActingUserCRUDServiceImpl.i().updadd(admin, dto);
 
 	}
 
