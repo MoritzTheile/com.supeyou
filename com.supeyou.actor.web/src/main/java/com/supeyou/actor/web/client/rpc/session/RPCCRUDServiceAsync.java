1aba2a690061dfa7799d82d382e65febeb992dcc
MoritzTheile <MoritzTheile@757eb1d2-deeb-4e35-a037-182455f4f83f>
2015-05-20 10:28:57 +0000

    saving work

---
+++ com.supeyou.actor.web/src/main/java/com/supeyou/actor/web/client/rpc/session/RPCCRUDServiceAsync.java
@@ -0,0 +1,12 @@
+package com.supeyou.actor.web.client.rpc.session;
+
+import com.google.gwt.core.client.GWT;
+import com.supeyou.crudie.iface.dto.GroupDTO;
+import com.supeyou.crudie.iface.dto.GroupFetchQuery;
+import com.supeyou.crudie.web.client.rpc.abstr.crud.RPCAbstractCRUDServiceAsync;
+
+public interface RPCCRUDServiceAsync extends RPCAbstractCRUDServiceAsync<GroupDTO, GroupFetchQuery> {
+
+	static final RPCCRUDServiceAsync i = GWT.create(RPCCRUDService.class);
+
+}
---
+++ com.supeyou.actor.web/src/main/java/com/supeyou/actor/web/client/rpc/session/choosersmall/item/ItemWidget.java
@@ -0,0 +1,96 @@
+package com.supeyou.actor.web.client.rpc.session.choosersmall.item;
+
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.ui.Label;
+import com.supeyou.crudie.iface.dto.GroupDTO;
+import com.supeyou.crudie.web.client.rpc.abstr.crud.RPCAbstractCRUDProxy.CRUDProxyListener;
+import com.supeyou.actor.web.client.rpc.session.RPCCRUDProxy;
+
+public class ItemWidget extends WidgetView {
+
+	private final ItemWidget thisWidget;
+
+	private GroupDTO thisDTO;
+
+	public ItemWidget(GroupDTO dto) {
+
+		this.thisDTO = dto;
+
+		RPCCRUDProxy.i().addListenerForAllDTOs(listener);
+
+		thisWidget = this;
+
+		render();
+
+	}
+
+	private void render() {
+
+		columnOne.clear();
+		columnOne.add(new Label(renderInfoString()));
+
+	}
+
+	private String renderInfoString() {
+
+		String infos = "";
+		if (thisDTO.getName() != null) {
+			infos += thisDTO.getName().value();
+		}
+
+		return infos;
+	}
+
+	private CRUDProxyListener<GroupDTO> listener = new CRUDProxyListener<GroupDTO>() {
+
+		@Override
+		public void wasUpdated(GroupDTO dto) {
+
+			final String updatedStyle = "updated";
+
+			thisWidget.removeStyleName(updatedStyle);
+
+			if (thisDTO.equals(dto)) {
+
+				thisDTO = dto;
+
+				// without the delay the change in style doesn't get picked up
+				new Timer() {
+
+					@Override
+					public void run() {
+
+						thisWidget.addStyleName(updatedStyle);
+
+					}
+				}.schedule(100);
+
+				render();
+			}
+		}
+
+		@Override
+		public void wasDeleted(GroupDTO dto) {
+
+			removeFromParent();
+
+		}
+
+		@Override
+		public void wasCreated(GroupDTO dto) {
+
+			// not possible
+
+		}
+
+	};
+
+	protected void onDetach() {
+
+		RPCCRUDProxy.i().removeListener(listener);
+
+		super.onDetach();
+
+	};
+
+}
