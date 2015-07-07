664165f816b513e81700fe123e9395e5c7f4e37f
MoritzTheile <MoritzTheile@757eb1d2-deeb-4e35-a037-182455f4f83f>
2015-07-07 10:36:34 +0000

    collapsing tree doesn't work correkt yet

--- com.supeyou.core.web/src/main/java/com/supeyou/core/web/client/view/invitationclicks/InvitationclicksWidget.java
+++ com.supeyou.core.web/src/main/java/com/supeyou/core/web/client/view/invitationclicks/InvitationclicksWidget.java
@@ -5,7 +5,10 @@ import java.util.List;
 
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.Node;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Label;
 import com.supeyou.core.iface.dto.SupporterDTO;
 import com.supeyou.core.iface.dto.SupporterFetchQuery;
 import com.supeyou.core.web.client.view.invitationclicks.edges.Edge;
@@ -23,51 +26,19 @@ public class InvitationclicksWidget extends WidgetView {
 
 	private final SupporterDTO supporterDTO;
 
+	public enum COLLAPSE_MODE {
+		COLLAPSED, EXPANDED
+	};
+
+	private COLLAPSE_MODE collapseMode = COLLAPSE_MODE.COLLAPSED;
+
 	public InvitationclicksWidget(final InvitationclicksWidget parentWidget, final SupporterDTO supporterDTO) {
 
 		thisWidget = this;
-		nameLabel.setText(supporterDTO.getComment().value());
 
 		this.supporterDTO = supporterDTO;
 
 		this.parentWidget = parentWidget;
-
-		SupporterFetchQuery supporterFetchQuery = new SupporterFetchQuery();
-
-		supporterFetchQuery.setInvitor(supporterDTO);
-
-		com.supeyou.core.web.client.rpc.supporter.RPCCRUDServiceAsync.i.fetchList(new Page(), supporterFetchQuery, new AsyncCallback<DTOFetchList<SupporterDTO>>() {
-
-			@Override
-			public void onFailure(Throwable caught) {
-
-				caught.printStackTrace();
-
-			}
-
-			@Override
-			public void onSuccess(DTOFetchList<SupporterDTO> childrenDTO) {
-
-				if (childrenDTO.isEmpty()) {
-
-					if (parentWidget != null) {
-						parentWidget.loadingAChildFinished(supporterDTO);
-					}
-
-				} else {
-
-					childrenStillLoading.addAll(childrenDTO);
-
-					for (final SupporterDTO childSupporterDTO : childrenDTO) {
-
-						childrenSlot.add(new InvitationclicksWidget(thisWidget, childSupporterDTO));
-
-					}
-
-				}
-
-			}
-		});
 
 	}
 
@@ -76,7 +47,20 @@ public class InvitationclicksWidget extends WidgetView {
 	 */
 	protected void renderEdges() {
 
-		EdgesWidget edgesWidget = new EdgesWidget(thisWidget.getOffsetWidth(), 30);
+		final EdgesWidget edgesWidget = new EdgesWidget(thisWidget.getOffsetWidth(), 30);
+
+		edgesWidget.addDomHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+
+				collapseMode = COLLAPSE_MODE.COLLAPSED;
+
+				render();
+
+			}
+
+		}, ClickEvent.getType());
 
 		int lowerEndpointFromLeft = 0;
 
@@ -98,9 +82,78 @@ public class InvitationclicksWidget extends WidgetView {
 		}
 
 		edgesWidget.render();
-
+		edgeSlot.clear();
 		edgeSlot.add(edgesWidget);
 
+	}
+
+	private void render() {
+
+		nameLabel.setText(supporterDTO.getComment().value());
+
+		edgeSlot.clear();
+		childrenSlot.clear();
+
+		if (COLLAPSE_MODE.EXPANDED.equals(collapseMode)) {
+
+			SupporterFetchQuery supporterFetchQuery = new SupporterFetchQuery();
+
+			supporterFetchQuery.setInvitor(supporterDTO);
+
+			com.supeyou.core.web.client.rpc.supporter.RPCCRUDServiceAsync.i.fetchList(new Page(), supporterFetchQuery, new AsyncCallback<DTOFetchList<SupporterDTO>>() {
+
+				@Override
+				public void onFailure(Throwable caught) {
+
+					caught.printStackTrace();
+
+				}
+
+				@Override
+				public void onSuccess(DTOFetchList<SupporterDTO> childrenDTO) {
+
+					if (childrenDTO.isEmpty()) {
+
+						if (parentWidget != null) {
+							parentWidget.loadingAChildFinished(supporterDTO);
+						}
+
+					} else {
+
+						childrenStillLoading.addAll(childrenDTO);
+
+						for (final SupporterDTO childSupporterDTO : childrenDTO) {
+
+							childrenSlot.add(new InvitationclicksWidget(thisWidget, childSupporterDTO));
+
+						}
+
+					}
+
+				}
+			});
+
+		} else {
+
+			Label expandButton = new Label("+");
+			expandButton.addClickHandler(new ClickHandler() {
+
+				@Override
+				public void onClick(ClickEvent event) {
+
+					collapseMode = COLLAPSE_MODE.EXPANDED;
+					render();
+
+				}
+			});
+
+			childrenSlot.add(expandButton);
+
+			if (parentWidget != null) {
+				parentWidget.loadingAChildFinished(supporterDTO);
+			}
+
+		}
 	}
 
 	protected void loadingAChildFinished(SupporterDTO childSupporterDTO) {
@@ -120,5 +173,11 @@ public class InvitationclicksWidget extends WidgetView {
 		}
 
 	};
+
+	@Override
+	protected void onLoad() {
+		render();
+		super.onLoad();
+	}
 
 }
