bb3c240e4824633e4721aa11f60c16b9f46d7dac
MoritzTheile <MoritzTheile@757eb1d2-deeb-4e35-a037-182455f4f83f>
2015-07-08 08:15:58 +0000

    renaming from invitationclicks to supporter

---
+++ com.supeyou.core.web/src/main/java/com/supeyou/core/web/client/view/supportertree/edges/Edge.java
@@ -0,0 +1,19 @@
+package com.supeyou.core.web.client.view.supportertree.edges;
+
+public class Edge {
+
+	private int distanceFromMiddle;
+
+	public Edge(int distanceFromMiddle) {
+		this.setDistanceFromMiddle(distanceFromMiddle);
+	}
+
+	public int getDistanceFromMiddle() {
+		return distanceFromMiddle;
+	}
+
+	public void setDistanceFromMiddle(int distanceFromMiddle) {
+		this.distanceFromMiddle = distanceFromMiddle;
+	}
+
+}
