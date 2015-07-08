391b5bcbd4e3907e523290757e52453c2a19bec8
MoritzTheile <MoritzTheile@757eb1d2-deeb-4e35-a037-182455f4f83f>
2015-07-08 08:15:58 +0000

    renaming from invitationclicks to supporter

---
+++ trunk/com.supeyou.core.web/src/main/java/com/supeyou/core/web/client/view/supportertree/edges/EdgesWidget.java
@@ -0,0 +1,49 @@
+package com.supeyou.core.web.client.view.supportertree.edges;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gwt.canvas.client.Canvas;
+import com.google.gwt.canvas.dom.client.Context2d;
+
+public class EdgesWidget extends WidgetView {
+
+	private Canvas canvas;
+
+	private List<Edge> edges = new ArrayList<>();
+
+	public EdgesWidget(int width, int height) {
+
+		canvas = Canvas.createIfSupported();
+		canvas.setCoordinateSpaceWidth(width);
+		canvas.setCoordinateSpaceHeight(height);
+		// canvas.setWidth("" + width);
+		// canvas.setHeight("" + height);
+		canvasSlot.add(canvas);
+
+	}
+
+	public void addNewEdge(Edge edge) {
+
+		edges.add(edge);
+
+	}
+
+	public void render() {
+
+		for (Edge edge : edges) {
+			Context2d context1 = canvas.getContext2d();
+			context1.moveTo(canvas.getCoordinateSpaceWidth() / 2, 0);
+			context1.bezierCurveTo(
+					canvas.getCoordinateSpaceWidth() / 2, canvas.getCoordinateSpaceHeight() / 2,
+					edge.getDistanceFromMiddle(), canvas.getCoordinateSpaceHeight() / 2,
+					edge.getDistanceFromMiddle(), canvas.getCoordinateSpaceHeight());
+
+			context1.setStrokeStyle("red");
+
+			context1.stroke();
+
+		}
+	}
+
+}
