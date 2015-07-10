40e8644a2c1bb0303c5bf0623c0fffafbb1563fb
MoritzTheile <MoritzTheile@757eb1d2-deeb-4e35-a037-182455f4f83f>
2015-07-10 11:21:38 +0000

    Invitation has a token now

--- com.supeyou.core.iface/src/main/java/com/supeyou/core/iface/dto/InvitationFetchQuery.java
+++ com.supeyou.core.iface/src/main/java/com/supeyou/core/iface/dto/InvitationFetchQuery.java
@@ -1,9 +1,20 @@
 package com.supeyou.core.iface.dto;
 
 import com.supeyou.crudie.iface.datatype.FetchQuery;
+import com.supeyou.crudie.iface.datatype.types.SingleLineString256Type;
 
 public class InvitationFetchQuery extends FetchQuery {
 
 	private static final long serialVersionUID = 4892062046L;
+
+	private SingleLineString256Type token;
+
+	public SingleLineString256Type getToken() {
+		return token;
+	}
+
+	public void setToken(SingleLineString256Type token) {
+		this.token = token;
+	}
 
 }
