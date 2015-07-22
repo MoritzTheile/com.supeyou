e2af39ac82c7e65d5761eca898f38dca4c6b998d
MoritzTheile <MoritzTheile@757eb1d2-deeb-4e35-a037-182455f4f83f>
2015-07-22 11:26:35 +0000

    added a media query to make body filling if width > 580px

--- trunk/com.supeyou.core.web/src/main/java/com/supeyou/core/web/client/mainmenu/MainMenuWidget.java
+++ trunk/com.supeyou.core.web/src/main/java/com/supeyou/core/web/client/mainmenu/MainMenuWidget.java
@@ -3,6 +3,7 @@ package com.supeyou.core.web.client.mainmenu;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 import com.supeyou.core.web.client.mainmenu.addhero.AddHeroWidget;
+import com.supeyou.core.web.client.resources.i18n.Text;
 import com.supeyou.core.web.client.rpc.hero.chooserlarge.ChooserLargeWidget;
 import com.supeyou.crudie.web.client.model.LoginStateModel;
 import com.supeyou.crudie.web.client.uiorga.menuanddisplay.MenuAndDisplay;
@@ -11,8 +12,8 @@ public class MainMenuWidget extends WidgetView {
 
 	public MainMenuWidget() {
 
-		final Label menuItem1 = new Label("Choose Hero");
-		final Label menuItem2 = new Label("Join As Hero");
+		final Label menuItem1 = new Label(Text.i.HEROSPAGE_ChooseHero());
+		final Label menuItem2 = new Label(Text.i.HEROSPAGE_JoinAsHero());
 		// final Label menuItem3 = new Label("The Mission");
 
 		MenuAndDisplay menuAndDisplay = new MenuAndDisplay(menu, display) {
--- trunk/com.supeyou.core.web/src/main/java/com/supeyou/core/web/client/view/landingpage/LandingPageWidget.java
+++ trunk/com.supeyou.core.web/src/main/java/com/supeyou/core/web/client/view/landingpage/LandingPageWidget.java
@@ -1,9 +1,30 @@
 package com.supeyou.core.web.client.view.landingpage;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.History;
+import com.supeyou.core.web.client.HistoryController.ANCHOR;
+import com.supeyou.core.web.client.resources.i18n.Text;
+import com.supeyou.crudie.web.client.uiorga.flatbutton.FlatButtonWidget;
 
 public class LandingPageWidget extends WidgetView {
 
 	public LandingPageWidget() {
+
+		FlatButtonWidget flatButtonWidget = new FlatButtonWidget();
+		flatButtonWidget.setText(Text.i.HEROSPAGE_ChooseHero());
+		flatButtonWidget.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+
+				History.newItem(ANCHOR.HEROS.name());
+
+			}
+		});
+
+		chooseHeroButtonSlot.add(flatButtonWidget);
+
 	}
 
 }
