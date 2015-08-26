b5e94ebc8d25e0937b34ac606d0d2dae7372b87d
MoritzTheile <MoritzTheile@757eb1d2-deeb-4e35-a037-182455f4f83f>
2015-08-26 12:17:26 +0000

    switched from paypal-sandbox to paypal-productive

--- com.supeyou.core.impl/src/main/java/com/supeyou/core/impl/initialdata/InitialCoreData.java
+++ com.supeyou.core.impl/src/main/java/com/supeyou/core/impl/initialdata/InitialCoreData.java
@@ -117,10 +117,10 @@ public class InitialCoreData {
 		user_Melanie = createUser("melanie@mtheile.com");
 		user_Marion = createUser("marion@mtheile.com");
 
-		hero_Moritz = createHero(user_Moritz, "Moritz Theile", "paypal.com-facilitator@moritztheile.de", "./heroPics/MoritzTheile.jpg", "http://mtheile.com", "SupeYou Developer", false);
-		hero_Martina = createHero(user_Martina, "Martina Fuchs", "paypal.com-facilitator@moritztheile.de", "./heroPics/MartinaFuchs.jpg", "http://moeglichkeits-coach.de", "SupeYou Coach", false);
-		hero_Nikolaus = createHero(user_Nikolaus, "Nikolaus Teixeira", "paypal.com-facilitator@moritztheile.de", "./heroPics/NikolausTeixeira.jpg", "http://willkommen-in-muenchen.de", "Engagiert sich für münchner Flüchtlinge", true);
-		hero_Tara = createHero(user_Tara, "Tara McCartney", "paypal.com-facilitator@moritztheile.de", "./heroPics/TaraMcCartney.jpg", "http://unitedforhope.org", "Dedicates her skills to empower India’s rural poor.", false);
+		hero_Moritz = createHero(user_Moritz, "Moritz Theile", "paypal.com@moritztheile.de", "./heroPics/MoritzTheile.jpg", "http://mtheile.com", "SupeYou Developer", false);
+		hero_Martina = createHero(user_Martina, "Martina Fuchs", "paypal.com@moritztheile.de", "./heroPics/MartinaFuchs.jpg", "http://moeglichkeits-coach.de", "SupeYou Coach", false);
+		hero_Nikolaus = createHero(user_Nikolaus, "Nikolaus Teixeira", "paypal.com@moritztheile.de", "./heroPics/NikolausTeixeira.jpg", "http://willkommen-in-muenchen.de", "Engagiert sich für münchner Flüchtlinge", true);
+		hero_Tara = createHero(user_Tara, "Tara McCartney", "paypal.com@moritztheile.de", "./heroPics/TaraMcCartney.jpg", "http://unitedforhope.org", "Dedicates her skills to empower India’s rural poor.", false);
 
 		rootSupporter_Moritz = createRootSupporter(hero_Moritz);
 		rootSupporter_Martina = createRootSupporter(hero_Martina);
