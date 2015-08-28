a6752a47b394992d86868752b27e5f3cb2989e85
MoritzTheile <MoritzTheile@757eb1d2-deeb-4e35-a037-182455f4f83f>
2015-08-28 16:45:25 +0000

    added postit for recommending email

---
+++ com.supeyou.core.web/src/main/java/com/supeyou/core/web/client/view/heropage/invite/recommendation/WidgetView.java
@@ -0,0 +1,26 @@
+package com.supeyou.core.web.client.view.heropage.invite.recommendation;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+public class WidgetView extends Composite {
+
+	private static ThisUiBinder uiBinder = GWT.create(ThisUiBinder.class);
+
+	@UiField
+	FlowPanel root;
+
+	interface ThisUiBinder extends UiBinder<Widget, WidgetView> {
+	}
+
+	public WidgetView() {
+
+		initWidget(uiBinder.createAndBindUi(this));
+
+	}
+
+}
