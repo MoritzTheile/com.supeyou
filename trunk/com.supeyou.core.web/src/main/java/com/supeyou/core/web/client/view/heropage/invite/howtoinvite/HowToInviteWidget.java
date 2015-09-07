ac176329f2011e9971a3c5d61a12b0383bd8fac3
MoritzTheile <MoritzTheile@757eb1d2-deeb-4e35-a037-182455f4f83f>
2015-09-07 07:40:59 +0000

    added "ask for email" widget (no function yet)

--- trunk/com.supeyou.core.web/src/main/java/com/supeyou/core/web/client/view/heropage/invite/howtoinvite/HowToInviteWidget.java
+++ trunk/com.supeyou.core.web/src/main/java/com/supeyou/core/web/client/view/heropage/invite/howtoinvite/HowToInviteWidget.java
@@ -9,6 +9,7 @@ import com.supeyou.core.iface.dto.SupporterDTO;
 import com.supeyou.core.web.client.resources.i18n.Text;
 import com.supeyou.core.web.client.rpc.invitation.RPCCRUDProxy;
 import com.supeyou.core.web.client.rpc.invitation.RPCCRUDServiceAsync;
+import com.supeyou.core.web.client.view.heropage.invite.askforemail.AskForEmailWidget;
 import com.supeyou.core.web.client.view.heropage.invite.hintmanual.HintManualWidget;
 import com.supeyou.core.web.client.view.heropage.invite.manually.ManuallyWidget;
 import com.supeyou.core.web.client.view.heropage.invite.manually.ManuallyWidget.RENDERMODE;
@@ -73,6 +74,8 @@ public abstract class HowToInviteWidget extends WidgetView {
 
 					}
 				});
+
+				askForEmail();
 
 			}
 
@@ -214,6 +217,25 @@ public abstract class HowToInviteWidget extends WidgetView {
 	public interface InvitationCallback {
 
 		void invitationCreated(InvitationDTO invitationDTO);
+
+	}
+
+	private void askForEmail() {
+
+		final PopupWidget popupWidget = new PopupWidget();
+
+		AskForEmailWidget contentWidget = new AskForEmailWidget(thisSupporterDTO) {
+
+			@Override
+			protected void onDismiss() {
+
+				popupWidget.closePopup();
+
+			}
+
+		};
+
+		popupWidget.init(new ContentWrapperWidget(Text.i.ASK_FOR_EMAIL_Header(), contentWidget));
 
 	}
 
