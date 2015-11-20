6b2a8918e9ec9820457b6bc3f39d1172af6de189
MoritzTheile <MoritzTheile@757eb1d2-deeb-4e35-a037-182455f4f83f>
2015-11-20 15:11:13 +0000

    the back button is functioning now

--- trunk/com.supeyou.core.web/src/main/java/com/supeyou/core/web/client/view/backbutton/BackButtonWidget.java
+++ trunk/com.supeyou.core.web/src/main/java/com/supeyou/core/web/client/view/backbutton/BackButtonWidget.java
@@ -1,6 +1,13 @@
 package com.supeyou.core.web.client.view.backbutton;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style.Visibility;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.user.client.History;
+import com.supeyou.core.web.client.HistoryController;
 
 public class BackButtonWidget extends WidgetView {
 
@@ -8,6 +15,39 @@ public class BackButtonWidget extends WidgetView {
 
 		supeYouLogo.setUrl(GWT.getModuleBaseForStaticFiles() + "images/SupeYou-Logo.png");
 
+		History.addValueChangeHandler(new ValueChangeHandler<String>() {
+
+			@Override
+			public void onValueChange(ValueChangeEvent<String> event) {
+				render();
+
+			}
+		});
+
+		this.addDomHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+
+				if (History.getToken() == null) {
+					History.newItem(HistoryController.ANCHOR.LP.name());
+				} else if (History.getToken().startsWith(HistoryController.ANCHOR.HERO.name() + "_")) {
+					History.newItem(HistoryController.ANCHOR.HEROES.name());
+				} else {
+					History.newItem(HistoryController.ANCHOR.LP.name());
+				}
+
+			}
+		}, ClickEvent.getType());
+
+		render();
 	}
 
+	private void render() {
+		if (HistoryController.ANCHOR.LP.name().equals(History.getToken())) {
+			iconSlot.getElement().getStyle().setVisibility(Visibility.HIDDEN);
+		} else {
+			iconSlot.getElement().getStyle().setVisibility(Visibility.VISIBLE);
+		}
+	}
 }
